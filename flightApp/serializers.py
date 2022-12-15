c18da0b739adebfa5c2cebf915d7533c5453692a
Baizel Kurian Varghese <baizelkv@gmail.com>
2022-12-15 21:14:12 +0530

    Serializers error fix

--- flightApp/serializers.py
+++ flightApp/serializers.py
@@ -4,14 +4,14 @@ from flightApp.models import Flight, Passenger, Reservation
 class FlightSerializer(serializers.ModelSerializer):
     class Meta:
         model = Flight
-        fileds='__all___'
+        fields = '__all__'
 
 class PassengerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Passenger
-        fileds='__all___'
+        fields = '__all__'
 
 class ReservationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Reservation
-        fileds='__all___'
+        fields = '__all__'
